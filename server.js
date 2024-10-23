5e41e1fc874940374fe5036e255bbb2e96d6abdd
Indravahini <145686434+Indravahini@users.noreply.github.com>
2024-10-23 21:09:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,12 +20,14 @@ app.use(cors());
 //     port: 3306
 // });
 
+app.use(express.json());
 const db = mysql.createConnection({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12739385',
-    password: 'XnCd3GGFdw',
-    database: 'sql12739385'
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
 
 db.connect(err => {
     if (err) {
